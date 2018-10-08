c9b62d5d375c89b32dee593ad167c527508f25b7
Trevor Takawira <trevortaks@gmail.com>
2018-10-08 10:33:31 +0200

    Minor changes(Adding Comments)

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask
-#from config import *
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_bootstrap import Bootstrap
@@ -10,22 +9,19 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 app = Flask(__name__)
-#app.config.from_object(SQLALCHEMY_DATABASE_URI)
+#Move to separate config file
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'eygfyiTYTdseGD5EE578OD098A7534RFDD' #For development purposes only
 db = SQLAlchemy(app)
 
-#from routes import *
 import routes
-#from models import Task, subTask
 from models import *
 
 migrate = Migrate(app, db)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-#db.create_all()
 
 if __name__ == '__main__':
     app.run(debug=True) #Dev environment
--- models.py
+++ models.py
@@ -1,5 +1,5 @@
-from datetime import datetime
 from app import app, db
+from datetime import datetime
 
 
 class Task(db.Model):
