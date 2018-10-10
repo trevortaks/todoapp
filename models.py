6af42d57ef20531a0e6e6a3a8294955d331786ee
Trevor Takawira <trevortaks@gmail.com>
2018-10-10 11:56:05 +0200

    User Login, Signup and Session Management: Too much code commited here

--- models.py
+++ models.py
@@ -1,5 +1,23 @@
-from app import app, db
+from app import db, app
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin, current_user
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'User'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(25), unique=True, index=True)
+    fullname = db.Column(db.String(256))
+    email = db.Column(db.String(256), unique=True, index=True)
+    password_hash = db.Column(db.String(128))
+    profilepic = db.Column(db.String(256))
+    task = db.relationship('Task', backref='user', lazy='dynamic', cascade = 'all, delete-orphan')
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 
 class Task(db.Model):
@@ -11,9 +29,11 @@ class Task(db.Model):
     finishdate = db.Column(db.DateTime, index = True)
     completed = db.Column(db.Boolean, default=False, nullable=False)
     subtasks = db.relationship('subTask', backref='task',cascade = 'all, delete-orphan', lazy = 'dynamic')
+    user_id = db.Column(db.Integer, db.ForeignKey('User.id'))
 
 class subTask(db.Model):
     __tablename__ = 'subTask'
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(128))
     task_id = db.Column(db.Integer, db.ForeignKey('Task.id'))
+
--- routes.py
+++ routes.py
@@ -1,17 +1,60 @@
-from flask import render_template, redirect, url_for
-from werkzeug.utils import secure_filename
-from datetime import datetime
-from models import Task, subTask
-from forms import newTask, Activity
-from app import app, db
 import os
 import uuid
+from datetime import datetime
+from flask import render_template, redirect, url_for
+from flask_login import login_required, login_user, logout_user, current_user
+#from werkzeug.utils import secure_filename
+from models import Task, subTask, User
+from forms import newTask, Activity, Login, SignUp
+from app import app, db
+
 
 
 @app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
+@login_required 
 def index():
-    tasks=Task.query.order_by(Task.timestamp.desc())
+    tasks=Task.query.filter_by(user_id=1).order_by(Task.timestamp.desc())
     return render_template('index.html', tasks=tasks)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = Login()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username = form.username.data).first_or_404()
+        if user is None or not user.check_password(form.password.data):
+            return redirect(url_for('login'))
+        login_user(user)
+        return redirect(url_for('index'))
+    return render_template('login.html', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = SignUp()
+    if form.validate_on_submit():
+        file = form.profilepic.data
+        filename = uuid.uuid4().hex
+        file.save(os.path.join(os.path.abspath("static/"),'photos', filename))
+        user = User(username = form.username.data,
+                    email = form.email.data,
+                    fullname = form.fullname.data,
+                    profilepic = filename
+                    )
+        user.set_password(form.password1.data)
+        
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('login'))
+    return render_template('signup.html', form=form)
 
 @app.route('/add', methods=['GET','POST'])
 def additem():
@@ -22,7 +65,8 @@ def additem():
         file.save(os.path.join(os.path.abspath("static/"), 'photos', filename))
         task = Task(name=form.name.data, 
                     finishdate = form.finishdate.data,
-                    image = filename
+                    image = filename,
+                    user = current_user
                     )
         db.session.add(task)
         db.session.commit()
@@ -32,8 +76,8 @@ def additem():
 
 
 @app.route('/delete/<id>')
-def deleteitem(id):
-    task = Task.query.filter_by(id = id).first_or_404()
+def deleteitem(task_id):
+    task = Task.query.filter_by(task_id=id).first_or_404()
     db.session.delete(task)
     db.session.commit()
     return redirect(url_for('index'))
